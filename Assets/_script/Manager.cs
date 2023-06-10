acedf1d729cb1fcedfedd168f9c9e84d158ea62c
Rakan Aljarallah <rakanaljarallah.96@gmail.com>
2023-06-10 09:34:51 +0300

    first-commit

---
+++ Assets/_script/Manager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void loadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
